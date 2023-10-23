452c37e3956352a9490b754b8e61f9dbd536f98b
PayToUse <67496494+PayToUse@users.noreply.github.com>
2023-10-23 10:29:32 +0800

    update follow points

--- src/ru/nsu/ccfit/zuev/osu/game/FollowTrack.java
+++ src/ru/nsu/ccfit/zuev/osu/game/FollowTrack.java
@@ -43,7 +43,7 @@ public class FollowTrack extends GameObject {
             region = ResourceManager.getInstance().getTexture("followpoint");
         }
         final float pointSize = region.getWidth() * scale;
-        int count = (int) ((dist - 64 * scale) / (pointSize * 1.1));
+        int count = (int) ((dist - 64 * scale) / (pointSize * 1.35));
         if (count > 0) {
             count--;
         }
