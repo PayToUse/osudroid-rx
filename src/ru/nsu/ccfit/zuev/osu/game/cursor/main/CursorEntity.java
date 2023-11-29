cd9ce935f8a2bc71c873ffbc57653ac8c9ff0541
PayToUse <67496494+PayToUse@users.noreply.github.com>
2023-11-29 13:45:52 +0800

    Update CursorEntity.java

--- src/ru/nsu/ccfit/zuev/osu/game/cursor/main/CursorEntity.java
+++ src/ru/nsu/ccfit/zuev/osu/game/cursor/main/CursorEntity.java
@@ -28,7 +28,7 @@ public class CursorEntity extends Entity {
             particleOffsetX = -trailTex.getWidth() / 2f;
             particleOffsetY = -trailTex.getHeight() / 2f;
 
-            var spawnRate = 35;
+            var spawnRate = 45;
 
             emitter = new PointParticleEmitter(particleOffsetX, particleOffsetY);
             particles = new CursorTrail(emitter, spawnRate, cursorSprite.baseSize, trailTex);
