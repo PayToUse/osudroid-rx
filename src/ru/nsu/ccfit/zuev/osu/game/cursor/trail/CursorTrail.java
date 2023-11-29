ddf84f60c728b67e8822630ed67db3e2124f79bb
PayToUse <67496494+PayToUse@users.noreply.github.com>
2023-11-29 13:39:39 +0800

    Update CursorTrail.java

--- src/ru/nsu/ccfit/zuev/osu/game/cursor/trail/CursorTrail.java
+++ src/ru/nsu/ccfit/zuev/osu/game/cursor/trail/CursorTrail.java
@@ -28,7 +28,7 @@ public class CursorTrail extends ParticleSystem {
     }
 
     private void fadeOut() {
-        addParticleModifier(new ExpireModifier(0.1f));
-        addParticleModifier(new AlphaModifier(1.0f, 0.0f, 0.0f, 0.2f));
+        addParticleModifier(new ExpireModifier(0.175f));
+        addParticleModifier(new AlphaModifier(1.0f, 0.0f, 0.0f, 0.35f));
     }
 }
